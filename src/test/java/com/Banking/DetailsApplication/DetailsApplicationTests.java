08c582ebf0622b636ba6b8a092af7436924a47aa
tapanPande <tapan.pande7@gmail.com>
2021-05-10 16:11:07 +0530

    Initial commit

---
+++ src/test/java/com/Banking/DetailsApplication/DetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Banking.DetailsApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
